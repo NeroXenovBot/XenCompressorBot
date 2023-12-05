0bc447c8ff6c6de7c6c896dcccc9f4202e11ddbd
NeroXenovBot <149504056+NeroXenovBot@users.noreply.github.com>
2023-12-06 00:10:56 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -20,14 +20,14 @@ except Exception as er:
 @bot.on(events.NewMessage(pattern="/start"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await start(e)
 
 
 @bot.on(events.NewMessage(pattern="/setcode"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await coding(e)
 
 
@@ -41,70 +41,70 @@ async def _(e):
 @bot.on(events.NewMessage(pattern="/showthumb"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await getthumb(e)
 
 
 @bot.on(events.NewMessage(pattern="/logs"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await getlogs(e)
 
 
 @bot.on(events.NewMessage(pattern="/cmds"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await zylern(e)
 
 
 @bot.on(events.NewMessage(pattern="/ping"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await up(e)
 
 
 @bot.on(events.NewMessage(pattern="/sysinfo"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await sysinfo(e)
 
 
 @bot.on(events.NewMessage(pattern="/leech"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await dl_link(e)
 
 
 @bot.on(events.NewMessage(pattern="/help"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await ihelp(e)
 
 
 @bot.on(events.NewMessage(pattern="/renew"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await renew(e)
 
 
 @bot.on(events.NewMessage(pattern="/clear"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await clearqueue(e)
 
 
 @bot.on(events.NewMessage(pattern="/speed"))
 async def _(e):
     if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+        return e.reply("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
     await test(e)
     
     
@@ -139,7 +139,7 @@ async def _(e):
 @bot.on(events.NewMessage(incoming=True))
 async def _(event):
         if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
-            return await event.reply_text("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg>○</a>Ask my Owner to get access: @Not_Xenov**")
+            return await event.reply_text("**Currently, You're not an Authorised User!!\n\n<a href="https://telegra.ph/file/b937f3f2b6509a10850f2.jpg">○</a>Ask my Owner to get access: @Not_Xenov**")
         if not event.photo:
             return
         os.system("rm thumb.jpg")
